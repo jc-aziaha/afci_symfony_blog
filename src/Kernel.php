9d65b8974d929efc3394eb2cec3c32f4b5ce4a51
Jean Claude <aziaha.formations@gmail.com>
2023-11-03 20:41:03 +0100

    Continuons.

---
+++ src/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+
+        \date_default_timezone_set("Europe/Paris");
+
+        parent::__construct($environment, $debug);
+    }
+
+
+}
